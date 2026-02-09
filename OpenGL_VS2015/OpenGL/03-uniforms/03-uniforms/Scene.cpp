6c65771f830272bc58bb8b91f429666ec802c1f6
nilhouses <nil.cases@gmail.com>
2026-02-09 21:02:29 +0100

    Ex3, falta 4 + textures

--- OpenGL_VS2015/OpenGL/03-uniforms/03-uniforms/Scene.cpp
+++ OpenGL_VS2015/OpenGL/03-uniforms/03-uniforms/Scene.cpp
@@ -34,15 +34,24 @@ void Scene::render()
 {
 	// Change quad colors using elapsed time
 	float value = (sin(currentTime / 1000.f) + 1.0f) / 2.0f;
+	// [Nil] canvis a l'escalat, amb un nou uniform 'scale', declarat al vertex shader
+	float s = 0.5f + value * 0.5f; // [0.5,1]
+	program.setUniform2f("scale", s, s);
 
+	// [Nil] canvis als colors, usant l'invers i jugant amb 0's i 1's
+	float invValue = 1.0f - value;
 	program.use();
-	program.setUniform4f("color", value, value, value, 1.0f);
+	// program.setUniform4f("color", value, value, value, 1.0f); // color -> negre
+	program.setUniform4f("color", invValue, invValue, invValue, 1.0f); // color -> blanc
 	quads[0]->render();
-	program.setUniform4f("color", value, 0, 0, 1.0f);
+	// program.setUniform4f("color", value, 0, 0, 1.0f); // color -> negre
+	program.setUniform4f("color", 1.0f, invValue, invValue, 1.0f); // color -> blanc
 	quads[1]->render();
-	program.setUniform4f("color", 0, value, 0, 1.0f);
+	// program.setUniform4f("color", 0, value, 0, 1.0f); // color -> negre
+	program.setUniform4f("color", invValue, 1.0f, invValue, 1.0f); // color -> blanc
 	quads[2]->render();
-	program.setUniform4f("color", 0, 0, value, 1.0f);
+	// program.setUniform4f("color", 0, 0, value, 1.0f); // color -> negre
+	program.setUniform4f("color", invValue, invValue, 1.0f, 1.0f); // color -> blanc
 	quads[3]->render();
 }
 
--- OpenGL_VS2015/OpenGL/03-uniforms/03-uniforms/shaders/simple.vert
+++ OpenGL_VS2015/OpenGL/03-uniforms/03-uniforms/shaders/simple.vert
@@ -1,8 +1,9 @@
 #version 330
 
 in vec2 position;
+uniform vec2 scale; // [Nil] nou uniform
 
 void main()
 {
-	gl_Position = vec4(position, 0.0, 1.0);
+	gl_Position = vec4(scale*position, 0.0, 1.0);
 }
