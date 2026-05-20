efc713da54e47badc622524c0a71806cb220bbdd
nilhouses <nil.cases@gmail.com>
2026-05-20 11:51:35 +0200

    minor pause menu change, now onto the lobby UI

--- 3DGame/Assets/Scripts/UI/ButtonHover.cs
+++ 3DGame/Assets/Scripts/UI/ButtonHover.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     private Vector3 originalScale;
     public float hoverScaleFactor = 1.1f;
